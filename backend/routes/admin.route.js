45ffec4f84dad523ad323998d7e064d5a5625f7d
Nasim Khan Milon <nasimkhanmilon@gmail.com>
2025-11-18 11:17:32 -0800

    Create admin login, add doctor controller for admin and also authenticate the admin

---
+++ backend/routes/admin.route.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { addDoctor, loginAdmin } from '../controllers/admin.controller.js'
+import upload from '../middlewares/multer.middleware.js'
+import authAdmin from '../middlewares/authAdmin.middleware.js'
+
+const adminRouter = express.Router()
+
+adminRouter.post('/add-doctor', authAdmin , upload.single('image') , addDoctor)
+adminRouter.post('/login', loginAdmin)
+
+export default adminRouter
