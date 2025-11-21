7cb8fff151198e7b7ce5d3e4e583091106141d03
Nasim Khan Milon <nasimkhanmilon@gmail.com>
2025-11-21 11:45:00 +0600

    Create Login.jsx and also set functionalities for admin login

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,6 +4,21 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   css: {
+    transformer: 'postcss' // force PostCSS instead of lightningcss
+  },
   plugins: [react(), tailwindcss()],
   server: {port:5174},
+  tailwindcss: {
+    theme: {
+      extend: {
+        colors: {
+          'primary': "#5f6fff",
+        },
+        gridTemplateColumns:{
+          'auto': 'repeat(auto-fill, minmax(200px, 1fr))'
+        }
+      },
+    },
+  },
 })
