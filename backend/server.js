e6b443d9c5cad7b062181aab18d72e579ee6db21
Nasim Khan Milon <nasimkhanmilon@gmail.com>
2025-11-22 19:14:07 +0600

    Crate a API to register user

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import adminRouter from './routes/admin.route.js'
 import doctorRouter from './routes/doctor.route.js'
+import userRouter from './routes/user.route.js'
 
 //app config
 const app = express()
@@ -19,6 +20,7 @@ app.use(cors())
 //api endpoints
 app.use('/api/admin', adminRouter)
 app.use('/api/doctor', doctorRouter)
+app.use('/api/user', userRouter)
 
 app.get('/', (req, res) => {
     res.send("API WORKING ")
