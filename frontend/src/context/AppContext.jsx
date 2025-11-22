a5e44a92361756bb870e3f8a2d51e1194c44092e
Nasim Khan Milon <nasimkhanmilon@gmail.com>
2025-11-23 01:47:27 +0600

    Integrate the  API to get user profile data in frontend

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -13,6 +13,7 @@ const AppContextProvider = (props) => {
     const [doctors, setDoctors] = useState([])
     // const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
     const [token, setToken] = useState(localStorage.getItem('token') || '')
+    const [userData, setUserData] = useState(false)
 
     const getDoctorData = async () => {
 
@@ -31,16 +32,42 @@ const AppContextProvider = (props) => {
         }
     }
 
+    const loadUserProfileData = async () => {
+        try {
+
+            const {data} = await axios.get(backendUrl + '/api/user/get-profile', {headers: {token}})
+            if(data.success) {
+                setUserData(data.userData)
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+            console.log(error)
+        }
+    }
+
     const value = {
         doctors, 
         currencySymbol,
         token, setToken,
-        backendUrl
+        backendUrl, 
+        userData, setUserData,
+        loadUserProfileData
     }
 
     useEffect(() => {
         getDoctorData()
     }, [])
+
+    useEffect(() => {
+        if(token) {
+            loadUserProfileData()
+        } else {
+            setUserData(false)
+        }
+    }, [token])
 
     return (
         <AppContext.Provider value={value}>
