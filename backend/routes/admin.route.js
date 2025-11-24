e1c766340be741d7e48e51154a7fb4085e574037
Nasim Khan Milon <nasimkhanmilon@gmail.com>
2025-11-24 13:38:26 +0600

    Create API  to get dashboard data for admin panel

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addDoctor, allDoctors, appointmentCancel, appointmentsAdmin, loginAdmin } from '../controllers/admin.controller.js'
+import { addDoctor, adminDashboard, allDoctors, appointmentCancel, appointmentsAdmin, loginAdmin } from '../controllers/admin.controller.js'
 import upload from '../middlewares/multer.middleware.js'
 import authAdmin from '../middlewares/authAdmin.middleware.js'
 import { changeAvailability } from '../controllers/doctor.controller.js'
@@ -12,5 +12,6 @@ adminRouter.post('/all-doctors', authAdmin, allDoctors)
 adminRouter.post('/change-availability', authAdmin, changeAvailability)
 adminRouter.get('/appointments', authAdmin, appointmentsAdmin)
 adminRouter.post('/cancel-appointment', authAdmin, appointmentCancel)
+adminRouter.get('/dashboard', authAdmin, adminDashboard)
 
 export default adminRouter
